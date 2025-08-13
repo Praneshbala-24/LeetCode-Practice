9320fb81aadb36c5d4cfbc93a06130642feb8718
Pranesh Bala <praneshbala24@gmail.com>
2025-08-13 19:18:31 +0530

    Time: 2 ms (52.31%) | Memory: 8.5 MB (91.85%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,7 +4,6 @@ public:
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-
         int reversedHalf = 0;
         while (x > reversedHalf) {
             reversedHalf = reversedHalf * 10 + x % 10;
